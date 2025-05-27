4837af990660b884a8cefc4568937f6d308e873d
Arailes <149024515+Arailes@users.noreply.github.com>
2025-05-27 04:10:23 +0000

    ygy

--- server/config/env.js
+++ server/config/env.js
@@ -1,8 +1,8 @@
 module.exports = {
-    API_URL: 'http://localhost:3001/api',
+    API_URL: 'https://upultimate-2.onrender.com/api',
     DATABASE: 'mongodb://mongo/easytrage',
     APP_PATH: '/usr/src/app',
     CONFIG_PATH: '/usr/src/app/config',
-    HTTPS_KEY: '/usr/src/app/config/easytrage-key.pem',
-    HTTPS_CERT: '/usr/src/app/config/easytrage-cert.pem'
+    HTTPS_KEY: 'config/easytrage-key.pem',
+    HTTPS_CERT: 'config/easytrage-cert.pem'
 }
