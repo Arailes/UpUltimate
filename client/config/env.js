a64649d4699c69d127784af7a3ea617dd5e35000
Arailes <149024515+Arailes@users.noreply.github.com>
2025-05-27 04:47:27 +0000

    api

--- client/config/env.js
+++ client/config/env.js
@@ -1,5 +1,5 @@
 module.exports = {
-    API_URL: 'http://localhost:3001',
+    API_URL: 'http://localhost:10000/',
     APP_PATH: '/usr/src/app',
     CONFIG_PATH: '/usr/src/app/config'
 }
--- server/config/env.js
+++ server/config/env.js
@@ -1,5 +1,5 @@
 module.exports = {
-    API_URL: 'http://localhost:3001/api',
+    API_URL: 'http://localhost:10000/api',
     DATABASE: 'mongodb://mongo/easytrage',
     APP_PATH: '/usr/src/app',
     CONFIG_PATH: '/usr/src/app/config',
